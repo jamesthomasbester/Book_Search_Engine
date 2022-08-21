5d3041be7cecaff13dbf3b23ba226531d323635d
James Bester <jamesthomasbester@gmail.com>
2022-08-21 12:59:47 +1000

    minor changes

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -16,27 +16,16 @@ import { SAVE_BOOK } from "../utils/mutations";
 import { useMutation } from "@apollo/react-hooks";
 
 const SearchBooks = () => {
-  // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
-  // create state for holding our search field data
   const [searchInput, setSearchInput] = useState("");
-
-  // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
-
-  // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
-  // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
-    // let isMounted = true; // note this flag denote mount status
     return () => {
       saveBookIds(savedBookIds);
-      // isMounted = false;
     };
   });
 
   const [saveBook, { error }] = useMutation(SAVE_BOOK);
-
-  // create method to search for books and set state on form submit
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
@@ -90,8 +79,6 @@ const SearchBooks = () => {
       if (!response) {
         throw new Error("something went wrong!");
       }
-
-      // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
     } catch (err) {
       console.error(err);
--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: true,
-});
+mongoose.connect("mongodb+srv://jamesbester:typo123@cluster0.jpy13lp.mongodb.net/?retryWrites=true&w=majority", 
+() => console.log("connected to db"),
+e => console.error(e)
+)
 
 module.exports = mongoose.connection;
