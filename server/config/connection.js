129c3f142148992b9da834b411110c4e9815dfe4
James Bester <jamesthomasbester@gmail.com>
2022-08-31 22:00:09 +1000

    test

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,11 @@
-const mongoose = require('mongoose');
 
-mongoose.connect(
-  'mongodb+srv://jamesbester:typo123@cluster0.jpy13lp.mongodb.net/?retryWrites=true&w=majority',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  },
-);
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://jamesbester:typo123@cluster0.jpy13lp.mongodb.net/?retryWrites=true&w=majority",
+{ useUnifiedTopology: true, 
+    useNewUrlParser: true
+}, 
+() => console.log("connected to db"),
+e => console.error(e)
+)
 
 module.exports = mongoose.connection;
